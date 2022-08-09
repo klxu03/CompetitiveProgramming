89b24b9879ed96c70ee58a5436be3c1c23bb568d
klxu03 <beaver2025@gmail.com>
2022-08-09 17:12:21 -0400

    Counting divisors and start of a pure math template

---
+++ CSES/counting-divisors.cpp
@@ -0,0 +1,97 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//Safe lowerbound for 1 second is 10^8 operations
+
+#define f0r(a, b) for (long long a = 0; a < b; a++)
+#define f1r(a, b, c) for (long long a = b; a < c; a++)
+#define isOdd & 1
+#define qpow2(exponent) 1 << exponent
+/* 2^exponent, because every time shifting bit to the leftBound you are essentially multiplying function by two */
+#define max3(a, b, c) max(a, max(b, c))
+#define pb push_back
+#define f first
+#define s second
+using ll = long long;
+
+#define mp make_pair
+#define t third
+#define pll pair<ll, ll>
+
+/* For Debugging Purposes */
+#ifdef LOCAL
+#define DEBUG(...) debug(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define DEBUG(...) 6
+#endif
+
+template<typename T, typename S> ostream& operator << (ostream &os, const pair<T, S> &p) {return os << "(" << p.first << ", " << p.second << ")";}
+template<typename C, typename T = decay<decltype(*begin(declval<C>()))>, typename enable_if<!is_same<C, string>::value>::type* = nullptr>
+ostream& operator << (ostream &os, const C &c) {bool f = true; os << "["; for (const auto &x : c) {if (!f) os << ", "; f = false; os << x;} return os << "]";}
+template<typename T> void debug(string s, T x) {cerr << "\033[1;35m" << s << "\033[0;32m = \033[33m" << x << "\033[0m\n";}
+template<typename T, typename... Args> void debug(string s, T x, Args... args) {for (int i=0, b=0; i<(int)s.size(); i++) if (s[i] == '(' || s[i] == '{') b++; else
+if (s[i] == ')' || s[i] == '}') b--; else if (s[i] == ',' && b == 0) {cerr << "\033[1;35m" << s.substr(0, i) << "\033[0;32m = \033[33m" << x << "\033[31m | "; debug(s.substr(s.find_first_not_of(' ', i + 1)), args...); break;}}
+
+#define io ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+ll n, m, q, Q, T, k, l, r, x, y, z, g;
+
+// Produce the prime factorization for the number n
+vector<int> factor(int n) {
+	vector<int> ret;
+	for (int i = 2; i * i <= n; i++) {
+		while (n % i == 0) {
+			ret.push_back(i);
+			n /= i;
+		}
+	}
+	if (n > 1) ret.push_back(n);
+	return ret;
+}
+
+void solve(); 
+
+// Problem: https://cses.fi/problemset/task/1713 
+int main() {
+	io;
+	ll test_cases = 1;
+	
+	f0r(test_case, test_cases) {
+		solve();
+	}
+}
+
+void solve() {
+	cin >> n;
+
+    f0r(i, n) {
+        int x;
+        cin >> x;
+        if (x > 1) {
+            vector<int> result = factor(x);
+            DEBUG(x, result);
+            ll ret = 1;
+
+            int curr = result[0];
+            int curr_counter = 1;
+            f1r(i, 1, result.size()) {
+                if (result[i] != curr) {
+                    ret *= curr_counter + 1;
+
+                    curr = result[i];
+                    curr_counter = 1;
+                } else {
+                    curr_counter++;
+                }
+            }
+
+            ret *= curr_counter + 1;
+
+            // Do stuff
+            cout << ret << endl;
+        } else {
+            cout << 1 << endl;
+        }
+    }
+}
