4ca9aae4232caae7214adeb3b5b95d2be7e83568
klxu03 <beaver2025@gmail.com>
2022-09-21 13:13:26 -0400

    Fixed coin-combinations-2 by making space O(x) instead of O(nx) but could also just use ints instead of long long to squeeze it right under the bounds

--- CSES/coin-combinations-2.cpp
+++ CSES/coin-combinations-2.cpp
@@ -54,35 +54,30 @@ int main() {
 void solve() {
 	cin >> n >> x;
     vector<ll> coins(n);
-    vector<vector<ll>> dp(n, vector<ll>(x + 1, 0)); // dp[n][x] means number of coin combinations that can build up a coin sum of x using any amount of the previous coins n including n
+    vector<ll> dp(x + 1, 0); // dp[x] means number of coin combinations that can build up a coin sum of x using any amount of the previous coins n going above
+
     f0r(i, n) {
         cin >> coins[i];
-        dp[i][0] = 1;
     }
 
     // Do first coin/first row
-    f1r(i, 1, x + 1) {
+    f0r(i, x + 1) {
         if (i % coins[0] == 0) {
-            dp[0][i] = 1;
+            dp[i] = 1;
         }
     }
 
     f1r(i, 1, n) {
         ll coin = coins[i];
         f1r(j, 1, x + 1) {
-            dp[i][j] += dp[i - 1][j];
-
             if (j - coin >= 0) {
-                dp[i][j] += dp[i][j - coin];
-                dp[i][j] %= MOD;
+                dp[j] += dp[j - coin];
+                dp[j] %= MOD;
             }
         }
     }
 
+    // DEBUG(dp);
 
-    // f0r(i, n) {
-    //     DEBUG(dp[i]);
-    // }
-
-	cout << dp[n - 1][x] << endl;
+	cout << dp[x] << endl;
 }
