79b1861a040d77c7139163ee962936f96ed5cc10
klxu03 <beaver2025@gmail.com>
2022-10-13 14:30:54 -0400

    Trying out a python solution

---
+++ Leetcode/medium/3.py
@@ -0,0 +1,13 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        myMap = {}
+        start = 0
+        res = 0
+
+        for i in range(len(s)):
+            if s[i] in myMap:
+                start = myMap[s[i]] + 1
+            res = max(res, i - start + 1)
+            myMap[s[i]] = i
+
+        return res           
