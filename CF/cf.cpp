34fb0abde0daa5560049b3b99bd22d6527a2ae1b
klxu03 <beaver2025@gmail.com>
2023-01-29 15:22:40 -0500

    CF Bsearch edu

--- CF/cf.cpp
+++ CF/cf.cpp
@@ -14,12 +14,13 @@ using namespace std;
 #define max3(a, b, c) max(a, max(b, c))
 #define pb push_back
 #define f first
-// #define s second
+#define s second
 using ll = long long;
 
 #define mp make_pair
 #define t third
 #define pll pair<ll, ll>
+#define pii pair<int, int>
 
 /* For Debugging Purposes */
 #ifdef LOCAL
@@ -46,146 +47,100 @@ void solve();
 int main() {
 	io;
 	ll test_cases = 1;
-	cin >> test_cases;
+	// cin >> test_cases;
 	
 	f0r(test_case, test_cases) {
 		solve();
 	}
 }
 
-#include <ext/pb_ds/assoc_container.hpp> 
-#include <ext/pb_ds/tree_policy.hpp>
+vector<int> arr;
+// leftmost element that is greater than or equal to val
+int binary_search_leftmost(int val) {
+	int l = 0; // left
+	int r = n - 1; // right
 
-using namespace __gnu_pbds; 
+	while (l < r) {
+		int mid = l + (r - l)/2;
 
-template <typename num_t>
-using ordered_set = tree<num_t, null_type, less<num_t>, rb_tree_tag, tree_order_statistics_node_update>;
-
-ll p;
-void solve() {
-	cin >> n >> p;
-	vector<ll> inp(n);
-
-	set<ll> inps; // numbers that have been seen
-	f0r(i, n) {
-		ll x;
-		cin >> x;
-		inp[i] = x;
-		inps.insert(x);
-	}
-	DEBUG(inp);
-
-	set<ll> s; // Set containing number "ranges" that need to be dealt with
-	// If set contains 0 5, then 0 1 2 3 4 5 are not present
-
-	// Insert in the smallest element possible starting at 0
-	// f0r(i, p - 1) {
-	// 	if (inps.count(i) == 0) {
-	// 		s.insert(i);
-	// 		break;
-	// 	}
-	// }
-
-	// Create the set s
-	f0r(i, n) {
-		bool prev_num = true; // insert previous number into set
-		bool next_num = true; // insert next number into set
-
-		ll prev_number = inp[i] - 1;
-		if (inp[i] == 0) {
-			prev_number = p - 1;
-		}
-		if (inps.count(prev_number) > 0) {
-			prev_num = false;
-		}
-		ll next_number = inp[i] + 1;
-		if (inp[i] == p - 1) {
-			next_number = 0;
-		}
-		if (inps.count(next_number) > 0) {
-			next_num = false;
-		}
-
-		if (prev_num) {
-			s.insert(prev_number);
-		}
-		if (next_num) {
-			s.insert(next_number);
-		}
-	}
-	DEBUG(s);
-
-	// Case where no overflow is required
-	if (s.size() == 0 || *(s.begin()) > inp[n - 1]) {
-		DEBUG("overflow not needed");
-		if (s.size() == 0) {
-			cout << 0 << endl;
+		if (arr[mid] >= val) {
+			r = mid;
 		} else {
-			// Attempt to add the biggest element
-			if (inps.count(p - 1) == 0) {
-				s.insert(p - 1);
-			}
-
-			// Since no overflow needed, just subtract the biggest end bound with last digit
-			cout << *(--s.end()) - inp[n - 1] << endl;
+			l = mid + 1;
 		}
-	} else {
-		DEBUG("overflow needed");
-		DEBUG(s);
-
-		// Simulate overflow happening
-		s.erase(0); // since we are overflowing, 0 is achieved
-		
-		if (n - 1 == 0) {
-			// manually erase 1 from "last" digit overflowing
-			s.erase(1);
-			inps.insert(1);
-
-			if (inps.count(2) == 0) {
-				s.insert(2);
-			}
-		}
-		r0f(i, n - 1) {
-			if (inp[i] != p - 1) {
-				// break overflowing continuing
-
-				// kill a lower bound, now see if you need to push up the lower bound
-				DEBUG("erase:", inp[i] + 1);
-				s.erase(inp[i] + 1);
-				inps.insert(inp[i] + 1); // a new number we have "visited"
-
-				if (inps.count(inp[i] + 2) == 0) {
-					s.insert(inp[i] + 2);
-				}
-				DEBUG(s);
-
-				break;
-			} else if (i == 0) {
-				// the last, or "first" digit is also p - 1 manually erase 1
-				s.erase(1);
-				inps.insert(1);	
-
-				if (inps.count(2) == 0) {
-					s.insert(2);
-				}
-			}
-
-			// else keep overflowing
-		}
-
-		ll max_num = 0; // max number less than pivot that we still haven't had yet
-		DEBUG("final", s);
-		for (ll num : s) {
-			DEBUG(num, inp[n - 1]);
-			if (num > inp[n - 1]) {
-				break;
-			}
-
-			max_num = num;
-		}
-		DEBUG(max_num, p - 1, inp[n - 1]);
-
-		// res = overflowed up amount + amount needed to begin overflowing
-		cout << (max_num + 1) + ((p - 1) - inp[n - 1]) << endl; 
 	}
+
+	// could instead just set r = n instead so its possible to assume value of n
+	if (arr[l] < val) {
+		l++; 
+	}
+
+	return l;
+}
+
+int binary_search_greater(int val) {
+	int l = 0; // left
+	int r = n - 1; // right
+
+	while (l < r) {
+		int mid = l + (r - l)/2;
+
+		if (arr[mid] > val) {
+			r = mid;
+		} else {
+			l = mid + 1;
+		}
+	}
+
+	// could instead just set r = n instead so its possible to assume value of n
+	if (arr[l] <= val) {
+		l++; 
+	}
+
+	return l;
+}
+
+// rightmost element that is less than or equal to val
+int binary_search_rightmost(int val) {
+	int l = 0; // left
+	int r = n - 1; // right
+ 
+	while (l < r) {
+		int mid = l + (r - l)/2;
+ 
+		if (arr[mid] <= val) {
+			l = mid + 1;
+		} else {
+			r = mid;
+		}
+	}
+
+	if (arr[l] > val) {
+		l--;
+	}
+ 
+	return l;
+}
+
+void solve() {
+	cin >> n;
+	arr = vector<int>(n);
+
+	f0r(i, n) {
+		cin >> arr[i];
+	}
+	sort(arr.begin(), arr.end());
+	DEBUG(arr);
+
+	cin >> q;
+	f0r(i, q) {
+		int inp_l, inp_r;
+		cin >> inp_l >> inp_r;
+		int l = binary_search_leftmost(inp_l);
+		int r = binary_search_greater(inp_r);
+
+		DEBUG(l, r);
+		cout << r - l << " ";
+	}
+	cout << endl;
 }
