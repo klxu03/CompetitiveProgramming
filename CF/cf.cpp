926af29f2339b4b8c85d0dec7dda8f200e690b44
klxu03 <beaver2025@gmail.com>
2023-05-04 08:49:27 -0400

    Another 2000 down

--- CF/cf.cpp
+++ CF/cf.cpp
@@ -61,80 +61,210 @@ int main() {
     }
 }
 
-bool recurse(ll height, ll width, multiset<pll, greater<pll>> &inp_h, multiset<pll, greater<pll>> &inp_w) {
-    DEBUG(height, width, inp_h, inp_w);
-    if (height == 0 || width == 0) return true;
+class DSU {
+public:
+    vector<int> c; // c for components
 
-    if ((*inp_w.begin()).f == width) {
-        pii popped_w = *inp_w.begin();
-        inp_w.erase(inp_w.begin());
+    DSU() {}
 
-        pii popped_h = {popped_w.s, popped_w.f};
-        inp_h.erase(inp_h.find(popped_h));
-
-        bool res = recurse(height - popped_w.s, width, inp_h, inp_w);
-        inp_w.insert(popped_w);
-        inp_h.insert(popped_h);
-        if (res) return true;
+    void init(int n) {
+        c = vector<int>(n, -1);
     }
 
-    if ((*inp_h.begin()).f == height) {
-        pii popped_h = *inp_h.begin();
-        inp_h.erase(inp_h.begin());
-
-        pii popped_w = {popped_h.s, popped_h.f};
-        inp_w.erase(inp_w.find(popped_w));
-
-        bool res = recurse(height, width - popped_h.s, inp_h, inp_w);
-        inp_h.insert(popped_h);
-        inp_w.insert(popped_w);
-        if (res) return true;
+    // get representive component (uses path compression)
+    int get(int x) {
+        return c[x] < 0 ? x : c[x] = get(c[x]);
     }
 
-    DEBUG("returning false");
-    return false;
+    bool same_set(int a, int b) {
+        return get(a) == get(b);
+    }
+
+    int size(int x) {
+        return -c[get(x)];
+    }
+
+    bool unite(int x, int y) {  // union by size
+        x = get(x), y = get(y);
+        if (x == y) return false; // same group
+        if (c[x] > c[y]) swap(x, y); // if c[x] is less negative, or greater than c[y]
+
+        c[x] += c[y];
+        c[y] = x;
+        return true;
+    }
+};
+DSU d;
+
+class UnweightedGraph {
+public:
+    long long nodes; // # of nodes
+    long long edges; // # of edges
+    bool undirected;
+    vector<vector<long long> > adj; // adjacency neighbor vector
+    vector<long long> visited; // visited nodes
+
+    UnweightedGraph() {}
+    vector<ll> indegree;
+    vector<ll> s_indegree; // summed indegree, so a DP running sum of indegrees from previous ones
+
+    void init(long long nodes, long long edges, bool undirected) {
+        this->nodes = nodes;
+        this->edges = edges;
+        this->undirected = undirected;
+
+        adj = vector<vector<long long> >(nodes);
+        visited = vector<long long>(nodes, false);
+
+        indegree = vector<ll>(nodes);
+        s_indegree = vector<ll>(nodes);
+    }
+
+    void add_adj(int from, int to) {
+        adj[from].pb(to);
+        indegree[to]++;
+    }
+
+    // handle the super indegree for the nodes along the starting path
+    // this handles cases of starting cycles by going backwards to a node along the path or other nodes that may make their way onto this path
+    void toposort() {
+        deque<long long> dq;
+
+        for (int i = 0; i < nodes; i++) {
+            if (indegree[i] == 0) {
+                dq.pb(i);
+            }
+        }
+
+        while(!dq.empty()) {
+            long long current = dq.front();
+            dq.pop_front();
+
+            for (long long neighbor : adj[current]) {
+                indegree[neighbor]--;
+                s_indegree[neighbor] += s_indegree[current] + 1;
+
+                if (indegree[neighbor] == 0) {
+                    dq.push_back(neighbor);
+                }
+            }
+        }
+    }
+};
+UnweightedGraph g;
+
+vector<int> inp;
+
+void fill_d(int start) {
+    int curr = start;
+    while (true) {
+        int diff = inp[curr];
+        if (diff == 0) break; // no hop, done infinite loop
+        if (curr + diff < 0 || curr + diff >= n) break; // done, broke out of 0 - (n - 1)
+        if (d.same_set(curr + diff, curr)) break; // done, repeating a loop back to same set
+        d.unite(curr, curr + diff);
+        g.add_adj(curr, curr + diff);
+        curr += diff;
+    }
 }
+
+vector<bool> visited;
+// determine if this start node ends up infinite looping or exiting
+int determine_end(int start) {
+    int curr = start;
+    visited[start] = true;
+    while (true) {
+        int diff = inp[curr];
+        if (curr + diff < 0 || curr + diff >= n) return 1; // done, broke out of 0 - (n - 1)
+        curr += diff;
+        if (visited[curr]) return -1; // already visited before, looping back
+        visited[curr] = true;
+    }
+}
+
+
+vector<bool> og_path;
+void get_og_path() {
+    int curr = 0;
+    og_path[curr] = true;
+    while (true) {
+        curr += inp[curr];
+        if (curr < 0 || curr >= n) break; // out of bounds, reached the end
+        if (og_path[curr]) break;
+        og_path[curr] = true;
+    }
+}
+
+ll num_exits; // the number of indices that exit naturally
 
 void solve() {
     cin >> n;
-    vector<pll> inp(n);
+    inp = vector<int>(n);
+    visited = vector<bool>(n);
+    og_path = vector<bool>(n);
+    num_exits = 0;
+    d.init(n);
+    DEBUG(inp, visited, og_path, num_exits, d.c, g.adj);
+    g.init(n, 0, false);
 
     f0r(i, n) {
-        cin >> inp[i].f >> inp[i].s;
+        cin >> inp[i];
     }
+    DEBUG(n, inp);
 
-    multiset<pll, greater<pll>> inp_h; // sorted greatest height down
-    multiset<pll, greater<pll>> inp_w; // sorted greatest height down
-
-    ll area = 0; // summed area
+    // init the dsu
     f0r(i, n) {
-        inp_h.insert(inp[i]);
-        inp_w.insert({inp[i].s, inp[i].f});
-        area += inp[i].f * inp[i].s;
-    }
-
-    DEBUG(area, *(inp_h.begin()), *inp_w.begin());
-
-    vector<pll> ans;
-    ll height = (*inp_h.begin()).f;
-    ll width = (*inp_w.begin()).f;
-
-    if (n == 1) {
-        ans.pb({height, width}); // only one rectangle, obvious
-    } else {
-        if (area % height == 0) {
-            bool res = recurse(height, area/height, inp_h, inp_w);
-            if (res) ans.pb({height, area/height});
-        }
-
-        if (area % width == 0) {
-            bool res = recurse(area/width, width, inp_h, inp_w);
-            if (res) ans.pb({area/width, width});
+        if (d.size(i) == 1) {
+            fill_d(i);
         }
     }
+    g.toposort();
 
-    cout << ans.size() << endl;
-    f0r(i, ans.size()) {
-        cout << ans[i].f << " " << ans[i].s << endl;
+    map<int, int> m; // map of group to status. [-1 if infinite loop, 1 if exits, 2 if unit]
+    f0r(i, n) {
+        m[d.get(i)] = 2;
     }
+
+    DEBUG(m);
+    for (auto p : m) {
+        m[p.f] = determine_end(p.f);
+        if (m[p.f] == 1) num_exits += d.size(p.f);
+    }
+    fill(visited.begin(), visited.end(), false);
+
+    ll ret = 0;
+    get_og_path();
+    bool initial_exits = m[d.get(0)] == 1;
+
+    // handle the og_path first
+    f0r(i, n) {
+        // initial one can point to any index that exits and go out of bounds n + 1 itself
+        // ^ this one gets handled naturally from the bottom stuff
+
+        // every point along the initial path can point to a node that exits (except going backwards along the path since that creates a new cycle) or n + 1 out itself
+        // do some math. if there are 5 nodes along this path, then the first one gets 4 choices forward, next one is 3, then 2, then 1, or like 4(4 + 1)/2. This math doesn't include other nodes not connected on the path that exit naturally or the n + 1 options of exiting
+        // actually this math should just be turned into super indegree from the graph g
+
+        if (og_path[i]) {
+            ret += n + 1 + num_exits;
+            if (initial_exits) {
+                ret -= 1 + g.s_indegree[i];
+            }
+            DEBUG(num_exits, g.s_indegree[i]);
+            DEBUG(i, ret);
+        }
+    }
+
+    // if initial one exits by itself every other index that is not directly on the path of the initial one can visit any 2 * n + 1 node
+    if (initial_exits) {
+        DEBUG("og exits");
+        f0r(i, n) {
+            if (og_path[i] == false) {
+                ret += 2 * n + 1;
+                DEBUG(i, ret);
+            }
+        }
+    }
+
+    cout << ret << endl;
 }
