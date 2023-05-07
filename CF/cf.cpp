5d8da2e0ebe2a78f65dd0047e14a26786586f20b
klxu03 <beaver2025@gmail.com>
2023-05-07 17:34:48 -0400

    Quick D solve

--- CF/cf.cpp
+++ CF/cf.cpp
@@ -7,7 +7,7 @@ using namespace std;
 #define f0r(a, b) for (long long a = 0; a < b; a++)
 #define f1r(a, b, c) for (long long a = b; a < c; a++)
 #define r0f(a, b) for (long long a = b - 1; a >= 0; a--)
-#define r1f(a, b, c) for (long long a = c - 1; a >= b; a--)
+#define r1f(a, b, c) for (long long a = b; a >= c; a--)
 #define isOdd & 1
 #define qpow2(exponent) 1 << exponent
 /* 2^exponent, because every time shifting bit to the leftBound you are essentially multiplying function by two */
@@ -64,62 +64,46 @@ int main() {
 void solve() {
     cin >> n;
     vector<ll> inp(n);
-
     f0r(i, n) {
         cin >> inp[i];
     }
 
-    vector<ll> dp(n, -1); // if the right ends here, what is the best 3 answers to the left
-
-    set<pair<ll, int>, greater<pair<ll, int>>> s; // set of {"summed value", ind}
-    vector<pair<ll, int>> cache(3);
-    s.insert({inp[0], 0});
-//    s.insert({inp[1] + 1, 1});
-    cache[0] = {inp[1] + 1, 1};
-    cache[1] = {inp[2] + 2, 2};
-    dp[2] = inp[0] + inp[1] + inp[2] - 2;
-    f1r(i, 3, n) {
-        cache[2] = {inp[i] + i, i};
-        DEBUG(i);
-        DEBUG(s, cache);
-
-        int counter = 0;
-        vector<pair<ll, int>> s_best(6, {-1, -1});
-        // get the top 3, and then their sums
-        for (auto p : s) {
-            s_best[counter] = p;
-            counter++;
-
-            if (counter == 3) break;
+    // store value, and then its index
+    vector<pair<ll, int>> pref(n);
+    pref[0] = {inp[0], 0};
+    f1r(i, 1, n) {
+        if (inp[i] + i > pref[i - 1].f) {
+            pref[i] = {inp[i] + i, i};
+        } else {
+            pref[i] = pref[i - 1];
         }
-
-        f0r(j, 3) {
-            s_best[3 + j] = {cache[j].f - j, cache[j].s};
-        }
-        sort(s_best.begin(), s_best.end(), greater<pair<ll, int>>());
-        DEBUG(s_best);
-        ll sum = 0;
-        int lowest_ind = INT_MAX;
-        f0r(j, 3) {
-            sum += inp[s_best[j].s];
-            lowest_ind = min(lowest_ind, s_best[j].s);
-        }
-        // compare s_best with cache
-        DEBUG(sum, lowest_ind);
-
-        // update the cache
-        s.insert(cache[0]);
-        cache[0] = cache[1];
-        cache[1] = cache[2];
-
-        dp[i] = sum - (i - lowest_ind);
     }
-    DEBUG(dp);
+    vector<pair<ll, int>> suff(n);
+    suff[n - 1] = {inp[n - 1], n - 1};
+    r1f(i, n - 2, 0) {
+        if (inp[i] + (n - 1) - i > suff[i + 1].f) {
+            suff[i] = {inp[i] + (n - 1) - i, i};
+        } else {
+            suff[i] = suff[i + 1];
+        }
+    }
+    DEBUG(pref);
+    DEBUG(suff);
 
+    // go through, select each point as a mid
     ll ret = -1;
-    f0r(i, n) {
-        ret = max(ret, dp[i]);
+    vector<ll> ans;
+    f1r(i, 1, n - 1) {
+        pair<ll, int> left = pref[i - 1];
+        pair<ll, int> right = suff[i + 1];
+
+        ll curr = inp[i] + inp[left.s] + inp[right.s];
+        DEBUG("init sum", curr);
+        curr -= (right.s - left.s);
+        ans.pb(curr);
+        ret = max(ret, curr);
     }
+    DEBUG(ans);
 
     cout << ret << endl;
 }
