6d4e750ac18acd8bc0af49223d7a42ad30e72ee4
klxu03 <beaver2025@gmail.com>
2023-05-27 10:58:57 -0700

    ACSL sample

--- CF/cf.cpp
+++ CF/cf.cpp
@@ -1,95 +1,136 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-using ll = long long;
-
-//Safe lowerbound for 1 second is 10^8 operations
 
 #define f0r(a, b) for (long long a = 0; a < b; a++)
 #define f1r(a, b, c) for (long long a = b; a < c; a++)
-#define r0f(a, b) for (long long a = b - 1; a >= 0; a--)
-#define r1f(a, b, c) for (long long a = b; a >= c; a--)
 #define pb push_back
 #define f first
 #define s second
-
-#define mp make_pair
-#define t third
-#define pll pair<ll, ll>
 #define pii pair<int, int>
-
-/* For Debugging Purposes */
-#ifdef LOCAL
-#define DEBUG(...) debug(#__VA_ARGS__, __VA_ARGS__)
-#else
-#define DEBUG(...) 6
-#endif
-
-template<typename T, typename S> ostream& operator << (ostream &os, const pair<T, S> &p) {return os << "(" << p.first << ", " << p.second << ")";}
-template<typename C, typename T = decay<decltype(*begin(declval<C>()))>, typename enable_if<!is_same<C, string>::value>::type* = nullptr>
-ostream& operator << (ostream &os, const C &c) {bool f = true; os << "["; for (const auto &x : c) {if (!f) os << ", "; f = false; os << x;} return os << "]";}
-template<typename T> void debug(string s, T x) {cerr << "\033[1;35m" << s << "\033[0;32m = \033[33m" << x << "\033[0m\n";}
-template<typename T, typename... Args> void debug(string s, T x, Args... args) {for (int i=0, b=0; i<(int)s.size(); i++) if (s[i] == '(' || s[i] == '{') b++; else
-        if (s[i] == ')' || s[i] == '}') b--; else if (s[i] == ',' && b == 0) {cerr << "\033[1;35m" << s.substr(0, i) << "\033[0;32m = \033[33m" << x << "\033[31m | "; debug(s.substr(s.find_first_not_of(' ', i + 1)), args...); break;}}
-
-/* Variartic Macros and Functions so max(a, b, c, d, ... z) works out of box */
-template<typename T> constexpr const inline T& _max(const T& x, const T& y) {return x<y?y:x;}
-template<typename T> constexpr const inline T& _min(const T& x, const T& y) {return x<y?x:y;}
-template<typename T,typename ...S>constexpr const inline T& _max(const T& m, const S&...s){return _max(m,_max(s...));}
-template<typename T,typename ...S>constexpr const inline T& _min(const T& m, const S&...s){return _min(m,_min(s...));}
-#define max(...) _max(__VA_ARGS__)
-#define min(...) _min(__VA_ARGS__)
-
 #define io ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-void usaco(string filename) {
-    io;
-    freopen((filename + ".in").c_str(), "r", stdin);
-    freopen((filename + ".out").c_str(), "w", stdout);
+
+int stooi(string num) {
+    int ret = 0;
+    if (num[0] == '-') {
+        f1r(i, 1, num.size()) {
+            ret *= 10;
+            ret += num[i] - '0';
+        }
+
+        ret *= -1;
+    } else {
+        f0r(i, num.size()) {
+            ret *= 10;
+            ret += num[i] - '0';
+        }
+    }
+
+    return ret;
 }
 
-#include <chrono>
-using namespace std::chrono;
-struct timer {
-    high_resolution_clock::time_point begin;
+// im assuming (2,2) isn't an interval, these are actual ones with values in them
+vector<pii> split(string interval) {
+    if (interval == "null") return {};
 
-    timer() {}
-    timer(bool b) {
-        if (b) start();
+    vector<pii> ret;
+    vector<string> v;
+    int i = 0;
+    while (i < interval.size()) {
+        string curr = "";
+        while (i < interval.size()) {
+            if (i == interval.size() - 1) {
+                curr += interval[i];
+                v.pb(curr);
+                i++;
+                break;
+            } else if (interval[i] == ' ') {
+                v.pb(curr);
+                i++;
+                break;
+            }
+            curr += interval[i];
+
+            i++;
+        }
     }
 
-    void start() {
-        begin = high_resolution_clock::now();
+    f0r(i, v.size()) {
+        string left = "";
+        string right = "";
+
+        int j;
+        // handle left
+        for (j = 1; j < v[i].size(); j++) {
+            if (v[i][j] == ',') {
+                j++;
+                break;
+            }
+            left.pb(v[i][j]);
+        }
+
+        for (j = j; j < v[i].size(); j++) {
+            if (v[i][j] == ')' || v[i][j] == ']') {
+                break;
+            }
+            right.pb(v[i][j]);
+        }
+
+        int left_n = stooi(left);
+        int right_n = stooi(right);
+
+        if (v[i][0] == '(') left_n++;
+        if (v[i][v[i].size() - 1] == ')') right_n--;
+        ret.pb({left_n, right_n});
+    }
+    sort(ret.begin(), ret.end());
+    return ret;
+}
+
+bool in_interval(int num, vector<pii> &interval) {
+    f0r(i, interval.size()) {
+        if (num >= interval[i].f && num <= interval[i].s) {
+            return true;
+        }
     }
 
-    void print() {
-        cout << "Time taken: " << duration_cast<duration<double>>(high_resolution_clock::now() - begin).count() << " seconds" << endl;
+    return false;
+}
+
+void sumsNotCommon(string intervals1, string intervals2, string intervals3) {
+    vector<pii> int1 = split(intervals1);
+    vector<pii> int2 = split(intervals2);
+    vector<pii> int3 = split(intervals3);
+
+    int in_one = 0;
+    int in_two = 0;
+
+    f1r(i, -99, 100) {
+        int cnt = 0; // # of intervals this num is in
+
+        if (in_interval(i, int1)) cnt++;
+        if (in_interval(i, int2)) cnt++;
+        if (in_interval(i, int3)) cnt++;
+
+        if (cnt == 1) {
+            in_one += i;
+        } else if (cnt == 2) {
+            in_two += i;
+        }
     }
 
-    double report() {
-        return duration_cast<duration<double>>(high_resolution_clock::now() - begin).count();
+    if (intervals3 == "null") {
+        cout << in_one << endl;
+    } else {
+        cout << in_one << " " << in_two << endl;
     }
-};
-// Start of main put tin, end of main put tpr (tgt gives you value not printed)
-#define tin timer __timer__(1);
-#define tpr __timer__.print();
-#define tgt __timer__.report()
+}
 
-ll q, Q, T, k, l, r, x, y, z;
-int n, m;
-
-void solve();
-
-// Problem URL:
 int main() {
     io;
-    long long test_cases = 1;
-    cin >> test_cases;
-
-    for (int i = 0; i < test_cases; i++) {
-        solve();
-    }
-}
-
-void solve() {
-    cin >> n;
+    string interval1, interval2, interval3;
+    getline(cin, interval1);
+    getline(cin, interval2);
+    getline(cin, interval3);
+    sumsNotCommon(interval1, interval2, interval3);
 }
