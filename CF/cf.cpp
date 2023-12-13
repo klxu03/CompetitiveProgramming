4ae7ac95bfbdefd7a0b3bf7afff7e4eabe6fee0a
klxu03 <beaver2025@gmail.com>
2023-12-12 20:20:21 -0500

    prac interview in

--- CF/cf.cpp
+++ CF/cf.cpp
@@ -1,182 +1,91 @@
+using namespace std;
 #include <bits/stdc++.h>
 
-using namespace std;
-using ll = long long;
+// https://codeforces.com/contest/1901/problem/D
 
-//Safe lowerbound for 1 second is 10^8 operations
+// 5 5 5 5 5
+// 2 4 5 3 1
 
-#define f0r(a, b) for (long long a = 0; a < b; a++)
-#define f1r(a, b, c) for (long long a = b; a < c; a++)
-#define r0f(a, b) for (long long a = b - 1; a >= 0; a--)
-#define r1f(a, b, c) for (long long a = c - 1; a >= b; a--)
-#define pb push_back
-#define f first
-#define s second
+// i fix index i, and I'm finding cost if chain left first, then right
+// bottle_neck_right + i (num of elements to the left)
+// bottle_neck_right = hp + dist from i
 
-#define mp make_pair
-#define t third
-#define pll pair<ll, ll>
-#define pii pair<int, int>
+// 1 1 1 1 1 1 1000 1 1 1 100
+// neg counter = 0
 
-/* For Debugging Purposes */
-#ifdef LOCAL
-#define DEBUG(...) debug(#__VA_ARGS__, __VA_ARGS__)
-#else
-#define DEBUG(...) 6
-#endif
-
-template<typename T, typename S> ostream& operator << (ostream &os, const pair<T, S> &p) {return os << "(" << p.first << ", " << p.second << ")";}
-template<typename C, typename T = decay<decltype(*begin(declval<C>()))>, typename enable_if<!is_same<C, string>::value>::type* = nullptr>
-ostream& operator << (ostream &os, const C &c) {bool f = true; os << "["; for (const auto &x : c) {if (!f) os << ", "; f = false; os << x;} return os << "]";}
-template<typename T> void debug(string s, T x) {cerr << "\033[1;35m" << s << "\033[0;32m = \033[33m" << x << "\033[0m\n";}
-template<typename T, typename... Args> void debug(string s, T x, Args... args) {for (int i=0, b=0; i<(int)s.size(); i++) if (s[i] == '(' || s[i] == '{') b++; else
-        if (s[i] == ')' || s[i] == '}') b--; else if (s[i] == ',' && b == 0) {cerr << "\033[1;35m" << s.substr(0, i) << "\033[0;32m = \033[33m" << x << "\033[31m | "; debug(s.substr(s.find_first_not_of(' ', i + 1)), args...); break;}}
-
-/* Variartic Macros and Functions so max(a, b, c, d, ... z) works out of box */
-template<typename T> constexpr const inline T& _max(const T& x, const T& y) {return x<y?y:x;}
-template<typename T> constexpr const inline T& _min(const T& x, const T& y) {return x<y?x:y;}
-template<typename T,typename ...S>constexpr const inline T& _max(const T& m, const S&...s){return _max(m,_max(s...));}
-template<typename T,typename ...S>constexpr const inline T& _min(const T& m, const S&...s){return _min(m,_min(s...));}
-#define max(...) _max(__VA_ARGS__)
-#define min(...) _min(__VA_ARGS__)
-
-#define io ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-void usaco(string filename) {
-    io;
-    freopen((filename + ".in").c_str(), "r", stdin);
-    freopen((filename + ".out").c_str(), "w", stdout);
-}
-
-#include <chrono>
-using namespace std::chrono;
-struct timer {
-    high_resolution_clock::time_point begin;
-
-    timer() {}
-    timer(bool b) {
-        if (b) start();
-    }
-
-    void start() {
-        begin = high_resolution_clock::now();
-    }
-
-    void print() {
-        cout << "Time taken: " << duration_cast<duration<double>>(high_resolution_clock::now() - begin).count() << " seconds" << endl;
-    }
-
-    double report() {
-        return duration_cast<duration<double>>(high_resolution_clock::now() - begin).count();
-    }
-};
-// Start of main put tin, end of main put tpr (tgt gives you value not printed)
-#define tin timer __timer__(1);
-#define tpr __timer__.print();
-#define tgt __timer__.report()
-
-mt19937_64 rng(std::chrono::steady_clock::now().time_since_epoch().count());
-// rng() gives a better random unsigned 32 bit number
-
-ll q, Q, T, k, l, r, x, y, z;
-int n, m;
-
-void solve();
-
-// Problem URL:
 int main() {
-    io;
-    long long test_cases = 1;
-     cin >> test_cases;
-
-    for (int i = 0; i < test_cases; i++) {
-        solve();
-    }
-}
-
-void solve() {
+    int n;
     cin >> n;
-    vector<ll> inp(n);
-    vector<ll> next_real_num(n, -1); // index of the next real num
 
-    int prev_real_num = 0;
-    ll tot_sum = 0;
-    f0r(i, n) {
+    vector<long long> inp(n);
+    for (int i = 0; i < n; i++) {
         cin >> inp[i];
-
-        tot_sum += inp[i];
-        if (inp[i] > 1) {
-            next_real_num[prev_real_num] = i;
-            prev_real_num = i;
-        }
     }
 
-    vector<ll> pref(n);
-    pref[0] = inp[0];
+    // handles edge case n == 1
+    if (n == 1) {
+        cout << inp[0] << endl;
+        return 0;
+    }
+
+    vector<long long> preprocess_right(n); // preprocess_right[j] gives me the index to the right of j that is going to be the bottleneck index
+    preprocess_right[n - 1] = -1;
+    long long threshold_val = inp[n - 1];
+    int ind = n - 1;
+    int neg_counter = -1;
+
+    for (int i = n - 2; i >= 0; i--) {
+        preprocess_right[i] = ind;
+
+        if (inp[i] + neg_counter >= threshold_val) {
+            threshold_val = inp[i] + neg_counter;
+            ind = i;
+        }
+        neg_counter--;
+    }
+
+    vector<long long> preprocess_left(n);
+    preprocess_left[0] = -1;
+    threshold_val = inp[0];
+    ind = 0;
+    neg_counter = -1;
+
     for (int i = 1; i < n; i++) {
-        pref[i] = pref[i - 1] + inp[i];
-    }
+        preprocess_left[i] = ind;
 
-    ll best = 1;
-    int begin_1s = 0;
-    int end_1s = 0;
-    for (int i = 0; i < n; i++) {
-        if (begin_1s == i && inp[i] == 1) {
-            begin_1s++;
+        if (inp[i] + neg_counter >= threshold_val) {
+            threshold_val = inp[i] + neg_counter;
+            ind = i;
         }
-
-        best *= inp[i];
+        neg_counter--;
     }
 
-    for (int i = n - 1; i >= 0; i--) {
-        if (inp[i] != 1) {
-            break;
-        }
+    long long ret = INT_MAX;
 
-        end_1s++;
+    // distance + hp
+    long long score_right = max(preprocess_right[0] + inp[preprocess_right[0]], inp[0]);
+    long long score_left = max((n - 1 - preprocess_left[n - 1]) + inp[preprocess_left[n - 1]], inp[n - 1]);
+    ret = min(score_right, score_left);
+
+    // power = 5, + 2 away so he effectively needs 7
+    // 5 5 5 5 5
+    // 5 6 9 8 7 <- wiz power
+
+    for (int i = 1; i < n - 1; i++) {
+        // assume index i is where we are starting
+
+        // min(ret, max(score going left, going right))
+        long long required_power_left = -1; // i first chain right, then go left
+        long long required_power_right = -1;
+
+        // 2 + 5 + 2
+        required_power_left = (n - i - 1) + inp[preprocess_left[i]] + (i - preprocess_left[i]);
+
+        // i els to the left
+        required_power_right = i + inp[preprocess_right[i]] + (preprocess_right[i] - i);
+
+        ret = min(ret, max(max(required_power_left, required_power_right), inp[i]));
     }
 
-    best += begin_1s + end_1s;
-
-    if (begin_1s == n) {
-        cout << 1 << " " << 1 << endl;
-        return;
-    }
-
-    pii best_pair = {begin_1s + 1, n - end_1s};
-    DEBUG(best, best_pair);
-
-    for (int i = 0; i < n; i++) {
-        // i marks the starting index, never start at a 1
-        if (inp[i] == 1) continue;
-        for (int size = 0; size <= 30; size++) {
-            // jump up to size forward real nums, with next_real_num jumping
-            // size = 1 means we are combining with 1 number, so 2 total
-
-            // calculate how many we've jumped, subtract the prefix sum in that area from tot_sum and add the product from jumping this much
-            int curr = i;
-            ll prod = inp[i];
-            for (int jump = 0; jump < size; jump++) {
-                int next = next_real_num[curr];
-                if (next == -1) {
-                    break;
-                }
-
-                prod *= inp[next];
-                curr = next; 
-            }
-
-            ll new_sum = prod + tot_sum - pref[curr];
-            // lower half of prefix sum
-            if (i > 0) {
-                new_sum -= pref[i - 1];
-            }
-
-            if (new_sum > best) {
-                best = new_sum;
-                best_pair = {i + 1, curr + 1};
-            }
-        }
-    }
-
-    cout << best_pair.f << " " << best_pair.s << endl;
+    cout << ret << endl;
 }
