d1913208d240e1049834e0b5ab2bc0998b3cc3cd
klxu03 <beaver2025@gmail.com>
2024-06-29 15:30:53 -0400

    blank template

--- CF/cf.cpp
+++ CF/cf.cpp
@@ -69,9 +69,16 @@ struct timer {
         return duration_cast<duration<double>>(high_resolution_clock::now() - begin).count();
     }
 };
+// Start of main put tin, end of main put tpr (tgt gives you value not printed)
+#define tin timer __timer__(1);
+#define tpr __timer__.print();
+#define tgt __timer__.report()
 
 mt19937_64 rng(std::chrono::steady_clock::now().time_since_epoch().count());
 // rng() gives a better random unsigned 32 bit number
+
+ll q, Q, T, k, l, r, x, y, z;
+int n, m;
 
 void solve();
 
@@ -87,180 +94,6 @@ int main() {
     }
 }
 
-class FindingBridges {
-public:
-    set<pii> bridges;
-    set<int> bridge_nodes;
-    void IS_BRIDGE(int v,int to) {
-        bridges.insert({v, to});
-        bridge_nodes.insert(v);
-        bridge_nodes.insert(to);
-    } // some function to process the found bridge
-
-    int n; // number of nodes
-    int m; // num edges
-    vector<vector<int>> adj; // adjacency list of graph
-    int num_visited;
-
-    vector<bool> visited;
-    vector<int> tin, low;
-    int timer;
-
-    vector<int> from;
-    FindingBridges(int n) {
-        this->n = n;
-        num_visited = 0;
-        adj = vector<vector<int>>(n);
-//        visited = vector<bool>(n, false);
-//        tin = vector<int>(n, -1);
-//        low = vector<int>(n, -1);
-
-        from = vector<int>(n, -1);
-    }
-
-    void init_adj() {
-        f0r(i, m) {
-            int a, b;
-            cin >> a >> b;
-            a--; b--;
-            adj[a].pb(b);
-            adj[b].pb(a);
-        }
-    }
-
-    void dfs(int v, int p = -1) {
-        visited[v] = true;
-        num_visited++;
-        tin[v] = low[v] = timer++;
-        bool parent_skipped = false;
-        for (int to : adj[v]) {
-            if (to == p && !parent_skipped) {
-                parent_skipped = true;
-                continue;
-            }
-            if (visited[to]) {
-                low[v] = min(low[v], tin[to]);
-            } else {
-                dfs(to, v);
-                low[v] = min(low[v], low[to]);
-                if (low[to] > tin[v]) {
-                    IS_BRIDGE(v, to);
-                }
-            }
-        }
-    }
-
-    void find_bridges() {
-        timer = 0;
-        visited.assign(n, false);
-        tin.assign(n, -1);
-        low.assign(n, -1);
-        for (int i = 0; i < n; ++i) {
-            if (!visited[i])
-                dfs(i);
-        }
-    }
-
-    int bfs(int start) {
-        // see how many nodes you can visit without crossing any bridge
-        // return the number of nodes visited
-
-        int ret = 0;
-
-        deque<int> q;
-        q.push_back(start);
-        set<int> visited;
-        visited.insert(start);
-        from[start] = -1;
-
-        while (!q.empty()) {
-            int node = q.front();
-            q.pop_front();
-            ret++;
-
-            for (int to : adj[node]) {
-                if (visited.count(to) || bridges.count({node, to}) || bridges.count({to, node})) {
-                    continue;
-                }
-
-                visited.insert(to);
-                from[to] = node;
-                q.push_back(to);
-            }
-        }
-
-        return ret;
-    }
-};
-
-int splitIntoTwoGroups(const std::vector<int>& nums) {
-    int n = nums.size();
-    int totalSum = std::accumulate(nums.begin(), nums.end(), 0);
-    int halfSum = totalSum / 2;
-
-    std::vector<std::vector<bool>> dp(n + 1, std::vector<bool>(halfSum + 1, false));
-    dp[0][0] = true;
-
-    for (int i = 1; i <= n; ++i) {
-        for (int j = halfSum; j >= nums[i - 1]; --j) {
-            dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i - 1]];
-        }
-        for (int j = 0; j <= halfSum; ++j) {
-            dp[i][j] = dp[i][j] || dp[i - 1][j];
-        }
-    }
-
-    int group1Sum = 0;
-    for (int j = halfSum; j >= 0; --j) {
-        if (dp[n][j]) {
-            group1Sum = j;
-            break;
-        }
-    }
-
-    std::vector<int> group1, group2;
-    int w = group1Sum;
-    for (int i = n; i > 0; --i) {
-        if (!dp[i - 1][w]) {
-            group1.push_back(nums[i - 1]);
-            w -= nums[i - 1];
-        } else {
-            group2.push_back(nums[i - 1]);
-        }
-    }
-
-    int group2Sum = totalSum - group1Sum;
-
-    for (int num : group1) {
-//        std::cout << num << " ";
-    }
-
-    for (int num : group2) {
-//        std::cout << num << " ";
-    }
-
-    return group1Sum;
-}
-
 void solve() {
-    int n, m;
-    cin >> n >> m;
-
-    FindingBridges fb(n);
-    fb.m = m;
-    fb.init_adj();
-
-    fb.find_bridges();
-
-    DEBUG(fb.bridges);
-
-    vector<int> val;
-    for (auto p : fb.bridge_nodes) {
-        val.pb(fb.bfs(p));
-//        DEBUG(fb.bfs(p));
-    }
-
-    int best = splitIntoTwoGroups(val);
-
-    cout << ((n - best) * (n - best - 1) + best * (best - 1)) / 2 << "\n";
+    cin >> n;
 }
